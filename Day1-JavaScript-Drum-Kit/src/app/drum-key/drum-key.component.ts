68680f536bfdc4ac8a7ef235109db471ace4a003
Connie <cleung334@gmail.com>
2017-09-01 00:16:52 +0800

    struggling to get flex box working in angular

--- Day1-JavaScript-Drum-Kit/src/app/drum-key/drum-key.component.ts
+++ Day1-JavaScript-Drum-Kit/src/app/drum-key/drum-key.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-drum-key',
+  selector: 'drum-key',
   templateUrl: './drum-key.component.html',
   styleUrls: ['./drum-key.component.css']
 })
 export class DrumKeyComponent implements OnInit {
 
+  @Input('value')
+  aciiValue: string;
+
+  @Input()
+  letter: string;
+
+  @Input()
+  description: string;
+
+  soundFile: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.soundFile = `assets/sounds/${this.description}.wav`;
+    console.log(this.soundFile);
+    console.log(this.aciiValue);
+
   }
 
 }
