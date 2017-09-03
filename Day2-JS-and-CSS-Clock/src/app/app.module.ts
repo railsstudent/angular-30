9fc17623bb1ecbb74ab408b304955f067fa2f615
Connie <cleung334@gmail.com>
2017-09-04 07:54:35 +0800

    Add CSS

--- Day2-JS-and-CSS-Clock/src/app/app.module.ts
+++ Day2-JS-and-CSS-Clock/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ClockComponent } from './clock/clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ClockComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
