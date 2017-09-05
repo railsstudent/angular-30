540c410cf46b30252a66c74db3301ec9d8ec4e31
Connie Leung <cleung334@gmail.com>
2017-09-05 22:44:54 +0800

    Update title by calling title service

--- Day3-CSS-Variables/src/app/app.component.ts
+++ Day3-CSS-Variables/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Scoped CSS Variables and JS';
+  
+  constructor(private titleService: Title) {
+      titleService.setTitle(this.title);
+  }
 }
