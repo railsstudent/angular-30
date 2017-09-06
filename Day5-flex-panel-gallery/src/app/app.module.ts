72fd4276dc23fa464bcf29446d2c4771c0fc8d4e
Connie Leung <cleung334@gmail.com>
2017-09-06 22:25:40 +0800

    Create panels and panel component

--- Day5-flex-panel-gallery/src/app/app.module.ts
+++ Day5-flex-panel-gallery/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PanelsComponent } from './panels/panels.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelsComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
