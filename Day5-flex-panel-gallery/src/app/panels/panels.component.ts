6f4de637d416ba9370d4a4264df316a68a9c43db
Connie Leung <cleung334@gmail.com>
2017-09-06 23:25:20 +0800

    Complete angular-05 Flex Panel Gallery

--- Day5-flex-panel-gallery/src/app/panels/panels.component.ts
+++ Day5-flex-panel-gallery/src/app/panels/panels.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  ViewChildren, 
+  QueryList, 
+  ElementRef, 
+  AfterViewInit 
+} from '@angular/core';
 
 @Component({
   selector: 'app-panels',
   templateUrl: './panels.component.html',
   styleUrls: ['./panels.component.css']
 })
-export class PanelsComponent implements OnInit {
+export class PanelsComponent implements AfterViewInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  @ViewChildren('mypanel')
+  panels: QueryList<ElementRef>;
+    
+  ngAfterViewInit() {
+    console.log(this.panels);
+    this.panels.forEach(panel => panel.nativeElement.addEventListener('click', 
+      function toggleOpen() {
+          this.classList.toggle('open');
+      }.bind(panel.nativeElement)) ); 
+         
+    this.panels.forEach(panel => panel.nativeElement.addEventListener('transitionend', 
+      function toggleActive(ev) {
+          const [propertyName='', classList=null] = [ev.propertyName, ev.target.classList];
+          if (propertyName.includes('flex')) {
+              classList.toggle('open-active');
+          }
+      }.bind(panel.nativeElement)) ); 
   }
-
 }
