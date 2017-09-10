54948903dc97dcadb7f8a85db352f48876dcaf65
Connie Leung <cleung334@gmail.com>
2017-09-10 11:04:37 +0800

    change title in each tab

--- Day8-Fun-with-HTML5-canvas/src/app/app.component.ts
+++ Day8-Fun-with-HTML5-canvas/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Title]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'HTML5 Canvas';
+  
+  constructor(titleService: Title) {
+    titleService.setTitle(this.title);
+  }
 }
