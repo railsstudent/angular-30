e24ec8b0fd266b19fa28a26f00f49439a37a79c1
Connie Leung <cleung334@gmail.com>
2017-09-11 22:43:34 +0800

    Create inbox and item components

--- Day10-Hold-Shift-And-Check-Checkboxes/src/app/inbox/inbox.component.ts
+++ Day10-Hold-Shift-And-Check-Checkboxes/src/app/inbox/inbox.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InboxComponent implements OnInit {
 
+  items = [];
+
   constructor() { }
 
   ngOnInit() {
+      this.items = [{
+                description: 'This is an inbox layout.'
+              },
+              {
+                description: 'Check one item'
+              },
+              { 
+                description: 'Hold down your Shift key'
+              },
+              {
+                description: 'Check a lower item'
+              },
+              {
+                description: 'Everything inbetween should also be set to checked'
+              },
+              {
+                description: 'Try do it with out any libraries'
+              },
+              {
+                description: 'Just regular JavaScript'
+              },
+              {
+                description: 'Good Luck!'
+              },
+              {
+                description: 'Don\'t forget to tweet your result!'
+              }
+            ];
   }
 
 }
