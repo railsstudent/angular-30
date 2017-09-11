5e22baed2856974881e3d50b36404a175b1970bf
Connie Leung <cleung334@gmail.com>
2017-09-11 22:45:36 +0800

    Change title of the page

--- Day10-Hold-Shift-And-Check-Checkboxes/src/app/app.component.ts
+++ Day10-Hold-Shift-And-Check-Checkboxes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hold Shift to Check Multiple Checkboxes';
+  
+  constructor(titleService: Title) {
+    titleService.setTitle(this.title);
+  }
 }
