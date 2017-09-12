30774e89a2424fbe45d704b980558c35620e0f0e
Connie Leung <cleung334@gmail.com>
2017-09-13 07:09:39 +0800

    Set title and css

---
+++ Day12-key-sequence-detection/src/app/konami/konami.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-konami',
+  templateUrl: './konami.component.html',
+  styleUrls: ['./konami.component.css']
+})
+export class KonamiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
