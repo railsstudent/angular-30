2b7e1b11bd935a89724229d3e1a81242591dfe6d
Connie Leung <cleung334@gmail.com>
2017-09-24 15:19:00 +0800

    Add component

---
+++ Day20-Speed-Recognition/src/app/speed-recognition/speed-recognition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speed-recognition',
+  templateUrl: './speed-recognition.component.html',
+  styleUrls: ['./speed-recognition.component.css']
+})
+export class SpeedRecognitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
