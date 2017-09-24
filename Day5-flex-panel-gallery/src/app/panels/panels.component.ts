1a6925cc8cfc59ff72558c1e02ca3806c810f24c
Connie <cleung334@gmail.com>
2017-09-24 21:42:19 +0800

    wip

--- Day5-flex-panel-gallery/src/app/panels/panels.component.ts
+++ Day5-flex-panel-gallery/src/app/panels/panels.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-panels',
   templateUrl: './panels.component.html',
   styleUrls: ['./panels.component.css']
 })
-export class PanelsComponent {
+export class PanelsComponent implements OnInit {
+  panelData = [];
 
+  ngOnInit() {
+    this.panelData = [
+      ['Hey', 'Let\'s', 'Dance'],
+      ['Give', 'Take', 'Receive'],
+      ['Experience', 'It', 'Today'],
+      ['Give', 'All', 'You can'],
+      ['Life', 'In', 'Motion']
+    ]
+  }
 }
