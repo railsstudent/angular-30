98922529c710f59b22819e24075735a345880469
Connie Leung <cleung334@gmail.com>
2017-09-24 20:35:23 +0800

    Complete angular 30. Day21 - speech detection

--- Day20-Speed-Recognition/src/app/speed-recognition/speed-recognition.component.ts
+++ Day20-Speed-Recognition/src/app/speed-recognition/speed-recognition.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Iwindow, SpeechStat } from '../shared/iwindow';
 
 @Component({
   selector: 'app-speed-recognition',
   templateUrl: './speed-recognition.component.html',
   styleUrls: ['./speed-recognition.component.css']
 })
-export class SpeedRecognitionComponent implements OnInit {
+export class SpeedRecognitionComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
+  recognition: any;
+  transcripts: SpeechStat[] = [];
+  
+  constructor(private zone: NgZone) { }
+ 
+  // https://hassantariqblog.wordpress.com/2016/12/04/angular2-web-speech-api-speech-recognition-in-angular2/
   ngOnInit() {
+      // window.SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+      const { SpeechRecognition, webkitSpeechRecognition }: Iwindow = <Iwindow>window;
+      const ActualSpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+      if (ActualSpeechRecognition) {
+        this.recognition = new ActualSpeechRecognition();
+        this.recognition.interimResult = true;
+        this.recognition.lang = 'en-us';
+        this.recognition.onend = () => {
+          this.recognition.start();
+        }
+        
+        this.recognition.onerror = e => {
+          console.error(e);
+        }
+        
+        this.recognition.onresult = e => {
+          console.log(e);
+          const stat: SpeechStat = Array.from(e.results)
+            .map(results => results[0])
+            .map(result =>  ({ transcript: result.transcript, confidence: result.confidence }))[0];
+          if (e.results[0].isFinal) {  
+             this.zone.run( () => {
+               this.transcripts.push(stat);
+             });
+          }
+        }      
+        this.recognition.start();
+      }
   }
-
+  
+  ngOnDestroy() {
+    if (this.recognition) {
+      this.recognition.stop();
+    }
+  }
 }
