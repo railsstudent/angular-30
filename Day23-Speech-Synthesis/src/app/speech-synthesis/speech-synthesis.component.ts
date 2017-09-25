ef66af85fbdc12e7fee8f6d093d863c451bef6cb
Connie Leung <cleung334@gmail.com>
2017-09-25 08:31:20 +0800

    WIP
    synthesis

--- Day23-Speech-Synthesis/src/app/speech-synthesis/speech-synthesis.component.ts
+++ Day23-Speech-Synthesis/src/app/speech-synthesis/speech-synthesis.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IWindow } from '../shared/iwindow';
+
+declare var SpeechSynthesisUtterance: any;
 
 @Component({
   selector: 'app-speech-synthesis',
@@ -10,6 +13,21 @@ export class SpeechSynthesisComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
+      console.log(<IWindow>window);
+      const { speechSynthesis }: IWindow = <IWindow>window;
+      //console.log(SpeechSynthesis, SpeechSynthesisUtterance); = 
+      console.log(speechSynthesis);
+      // if (window.speechSynthesis) {
+      //   console.log('hello');
+      // }
+      /*if (window.SpeechSynthesisUtterance) {
+        console.log('world');
+      }*/
+      if (SpeechSynthesisUtterance) {
+        const x = new SpeechSynthesisUtterance();
+        console.log(x);
+        console.log('world');
+      }
   }
   
   ngOnDestroy() {
