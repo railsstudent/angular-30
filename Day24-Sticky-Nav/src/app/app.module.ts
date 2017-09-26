3005828db5e8e3d4ac36978abec5b5325c6af31c
Connie Leung <cleung334@gmail.com>
2017-09-27 07:34:27 +0800

    Add navbar component

--- Day24-Sticky-Nav/src/app/app.module.ts
+++ Day24-Sticky-Nav/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
