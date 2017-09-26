c598533623942af22b85a752a9074ee66aa502a7
Connie Leung <cleung334@gmail.com>
2017-09-27 00:42:09 +0800

    angular 30 Day22 Follow Along Nav

--- Day22-Follow-Along-Link-Highlighter/src/app/follow-along-nav/follow-along-nav.component.ts
+++ Day22-Follow-Along-Link-Highlighter/src/app/follow-along-nav/follow-along-nav.component.ts
@@ -11,47 +11,31 @@ export class FollowAlongNavComponent implements OnInit, OnDestroy {
   @ViewChild('myhighlight')
   highlight: ElementRef;
 
-  callback =  [];
+  callback: any;
+  links: any;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
+    const self = this;
+    this.callback =  function() {
+      const linkCoords = this.getBoundingClientRect();
+      const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+      };
+      self.renderer.setElementStyle(self.highlight.nativeElement, 'width', `${coords.width}px`);
+      self.renderer.setElementStyle(self.highlight.nativeElement, 'height', `${coords.height}px`);
+      self.renderer.setElementStyle(self.highlight.nativeElement, 'transform', `translate(${coords.left}px, ${coords.top}px)`);
+    }
   }
 
   ngOnInit() {
-    const links = this.elementRef.nativeElement.querySelectorAll('a');
-
-
-    function xxx() {
-        console.log(a.getBoundingClientRect());
-        const linkCoords = a.getBoundingClientRect();
-        console.log(linkCoords);
-        const coords = {
-          width: linkCoords.width,
-          height: linkCoords.height,
-          top: linkCoords.top + window.scrollY,
-          left: linkCoords.left + window.scrollX
-        };
-        this.renderer.setElementStyle(this.highlight.nativeElement, 'width', `${coords.width}px`);
-        this.renderer.setElementStyle(this.highlight.nativeElement, 'height', `${coords.height}px`);
-        this.renderer.setElementStyle(this.highlight.nativeElement, 'transform', `translate(${coords.left}px, ${coords.top}px)`);
-    }
-
-    links.forEach(a => a.addEventListener('mouseenter', () => {
-        console.log(a.getBoundingClientRect());
-        const linkCoords = a.getBoundingClientRect();
-        console.log(linkCoords);
-        const coords = {
-          width: linkCoords.width,
-          height: linkCoords.height,
-          top: linkCoords.top + window.scrollY,
-          left: linkCoords.left + window.scrollX
-        };
-        this.renderer.setElementStyle(this.highlight.nativeElement, 'width', `${coords.width}px`);
-        this.renderer.setElementStyle(this.highlight.nativeElement, 'height', `${coords.height}px`);
-        this.renderer.setElementStyle(this.highlight.nativeElement, 'transform', `translate(${coords.left}px, ${coords.top}px)`);
-    }));
+    this.links = this.elementRef.nativeElement.querySelectorAll('a');
+    this.links.forEach(a => a.addEventListener('mouseenter', this.callback.bind(a)));
   }
 
   ngOnDestroy() {
-
+    this.links.forEach(a => a.removeEventListener('mouseenter', this.callback))
   }
 }
