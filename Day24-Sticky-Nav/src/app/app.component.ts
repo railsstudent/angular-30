78b27e8baf3ccdabab5b3c074ae5e811e4956983
Connie <cleung334@gmail.com>
2017-09-29 05:34:44 +0800

    angular 30 - Complete Day 24 Sticky Nav

--- Day24-Sticky-Nav/src/app/app.component.ts
+++ Day24-Sticky-Nav/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, HostListener, ViewChild, ElementRef, OnInit, Renderer } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef, OnInit, Renderer, Inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +15,12 @@ export class AppComponent implements OnInit {
   @ViewChild(NavComponent)
   navComponent: NavComponent;
 
+  @ViewChild(ContentComponent)
+  contentComponent: ContentComponent;
+
   topOfNav: number;
 
-  constructor (titleService: Title, private renderer: Renderer) {
+  constructor (titleService: Title, private renderer: Renderer, @Inject(DOCUMENT) private document: any) {
     titleService.setTitle(this.title);
   }
 
@@ -28,5 +33,11 @@ export class AppComponent implements OnInit {
     const currentOffsetTop = this.navComponent.getOffsetTop();
     console.log(currentOffsetTop, window.scrollY);
     this.navComponent.setFixedNav(window.scrollY >= this.topOfNav);
+    this.contentComponent.setFixedNav(window.scrollY >= this.topOfNav);
+    if (window.scrollY >= this.topOfNav) {
+      this.renderer.setElementStyle(this.document.body, 'paddingTop', '70px');
+    } else {
+      this.renderer.setElementStyle(this.document.body, 'paddingTop', '0');
+    }
   }
 }
--- Day24-Sticky-Nav/src/app/content/content.component.ts
+++ Day24-Sticky-Nav/src/app/content/content.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
+  host: {
+      '[class.fixed-nav]': 'showStickyNav'
+  }
 })
 export class ContentComponent implements OnInit {
+
+  showStickyNav: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  setFixedNav(showStickyNav) {
+    this.showStickyNav = showStickyNav;
+  }
 }
