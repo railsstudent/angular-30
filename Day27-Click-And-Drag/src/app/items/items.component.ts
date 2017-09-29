94c8736a586deb07a5d52eb0a2fbcc2463049b38
Connie <cleung334@gmail.com>
2017-09-30 07:54:39 +0800

    Angular 30 - Complete Day 27 Click and Drag

--- Day27-Click-And-Drag/src/app/items/items.component.ts
+++ Day27-Click-And-Drag/src/app/items/items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-items',
@@ -8,13 +8,48 @@ import { Component, OnInit } from '@angular/core';
 export class ItemsComponent implements OnInit {
 
   numbers: number[];
+  isDown: boolean = false;
+  startX: number;
+  scrollLeft: number;
 
-  constructor() { }
+  @ViewChild('items')
+  slider: ElementRef;
+
+  constructor(private renderer: Renderer) { }
 
   ngOnInit() {
-    const numbers = Array(25).fill(1);
+    const numbers = Array(30).fill(1);
     this.numbers = numbers.map((n, i) => i + 1);
     console.log(this.numbers);
   }
 
+  onMouseDown($event) {
+    console.log('onMouseDown fired');
+    const {pageX} = $event;
+    this.isDown = true;
+    this.renderer.setElementClass(this.slider.nativeElement, 'active', true);
+    this.startX = pageX - this.slider.nativeElement.offsetLeft;
+    this.scrollLeft = this.slider.nativeElement.scrollLeft;
+  }
+
+  onMouseLeave() {
+    console.log('onMouseLeave fired');
+
+    this.isDown = false;
+    this.renderer.setElementClass(this.slider.nativeElement, 'active', false);
+  }
+
+  onMouseMove($event) {
+    const {pageX, target} = $event;
+    if (!this.isDown) {
+      return;
+    }
+    console.log('onMouseMove fired');
+
+    $event.preventDefault();
+    const x = pageX - this.slider.nativeElement.offsetLeft;
+    const walk = (x - this.startX) * 3; // how far we deviate from the initial point
+    console.log(walk);
+    this.renderer.setElementProperty(this.slider.nativeElement, 'scrollLeft', this.scrollLeft - walk);
+  }
 }
