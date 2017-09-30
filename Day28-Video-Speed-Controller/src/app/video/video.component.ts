fb019fb845b00d2180a2e0da428a9d115b020912
Connie Leung <cleung334@gmail.com>
2017-10-01 07:47:53 +0800

    angular30 - complete day28 video speed controller

--- Day28-Video-Speed-Controller/src/app/video/video.component.ts
+++ Day28-Video-Speed-Controller/src/app/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer } from '@angular/core';
 
 @Component({
   selector: 'app-video',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer) { }
 
   ngOnInit() {
   }
 
+  onMouseMove($event, speed, bar, video) {
+    const {pageY} = $event;
+    const {offsetTop, offsetHeight} = speed;
+    const y = pageY - offsetTop;
+    const percent = y / offsetHeight;
+
+    const min = 0.4;
+    const max = 4;
+    const height = `${Math.round(percent * 100)}%`;
+    const playbackRate = percent * (max - min) + min;
+
+    this.renderer.setElementStyle(bar, 'height', height);
+    this.renderer.setElementProperty(bar, 'textContent', `${playbackRate.toFixed(2)}x`);
+    video.playbackRate = playbackRate;
+  }
 }
