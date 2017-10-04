377ae0ddc76bc16f56e00851401195796f699552
Connie <cleung334@gmail.com>
2017-10-04 08:10:28 +0800

    Add functionality of video

--- Day11-Custom-Video-Player/src/app/video-player/video-player.component.ts
+++ Day11-Custom-Video-Player/src/app/video-player/video-player.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoPlayerComponent implements OnInit {
 
+  mousedown: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.mousedown = false;
+  }
+
+  handleRangeUpdate({target}, video) {
+    const {name, value} = target;
+    console.log(name, value);
+    video[name] = value;
   }
 
 }
