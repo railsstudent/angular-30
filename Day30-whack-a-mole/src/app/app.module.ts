74b7b7c3d00e18b058227c138aea1e8cb7efe2bc
Connie Leung <cleung334@gmail.com>
2017-10-09 08:09:52 +0800

    Update tab page title and create game component

--- Day30-whack-a-mole/src/app/app.module.ts
+++ Day30-whack-a-mole/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
