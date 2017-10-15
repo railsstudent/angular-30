a3e709315f1e42f2df2c7f946113fb882e504fda
Connie Leung <cleung334@gmail.com>
2017-10-16 07:22:32 +0800

    Add speedometer component

--- Day21-Geolocation/src/app/app.module.ts
+++ Day21-Geolocation/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpeedometercdComponent } from './speedometercd/speedometercd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpeedometercdComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Day21-Geolocation/src/app/speedometercd/speedometercd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speedometercd',
+  templateUrl: './speedometercd.component.html',
+  styleUrls: ['./speedometercd.component.css']
+})
+export class SpeedometercdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
